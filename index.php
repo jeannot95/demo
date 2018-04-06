d50d1e6c7a8934221f6f3dc1aecc8e1fd6c52ab1
Jean AUDOIN <audoinjean95@hotmail.com>
2018-04-06 10:39:01 +0200

    ajoutcss

--- index.php
+++ index.php
@@ -4,8 +4,19 @@
 	<head>
 		<title>Site démo</title>
 	</head>
+		<style>
+			h1{
+				position : relative;
+				top : 50%;
+				left:50%;		
+				color : lightblue;
+			}
+		
+		
+		</style>
+	
 	<body>
-		<h1>Hello</h1>
+		<h1>Hello everybody c'est vendredi bientôt en week-end</h1>
 	</body>
 </html>
 
