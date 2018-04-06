81f8a95595f9121d017068a31713b6ad526e9947
Jean AUDOIN <audoinjean95@hotmail.com>
2018-04-06 11:31:26 +0200

    h2

--- index.php
+++ index.php
@@ -15,10 +15,12 @@
 				margin-left : 50px;
 				margin-right:50px;
 			}
+			h2{color :red}
 		</style>
 	
 	<body>
 		<h1>Hello everybody c'est vendredi bientôt en week-end</h1>
+		<h2> VIvement ce soir</h2>
 		<p>Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end </P>
 	</body>
 </html>
