6d067766d11ad8b6d5e7c5acb5374d6a45988b93
Jean AUDOIN <audoinjean95@hotmail.com>
2018-04-06 11:16:30 +0200

    change

--- index.php
+++ index.php
@@ -7,14 +7,19 @@
 		<style>
 			h1{
 				position : relative;
-				top : 50%;
-				left:50%;		
+				top : 35%;
+				left:25%;		
 				color : lightblue;
+			}
+			p{
+				margin-left : 50px;
+				margin-right:50px;
 			}
 		</style>
 	
 	<body>
 		<h1>Hello everybody c'est vendredi bientôt en week-end</h1>
+		<p>Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end </P>
 	</body>
 </html>
 
