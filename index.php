06564961bfa2ad3c1c7ddedc3ed490435edff24b
Jean AUDOIN <audoinjean95@hotmail.com>
2018-04-06 11:05:37 +0200

    ajout css

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<title>Site démo</title>
 	</head>
 	<body>
-		<h1>Hello everybody c'est vendredi bientôt le week-end</h1>
+		<h1>Hello everybody c'est vendredi bientôt en week-end</h1>
 	</body>
 </html>
 
