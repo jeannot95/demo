4f09ea7ccfe2bac1184887df5e7bf7133eccb6d8
Jean AUDOIN <audoinjean95@hotmail.com>
2018-04-06 11:36:11 +0200

    2eme paragraphe

--- index.php
+++ index.php
@@ -22,6 +22,8 @@
 		<h1>Hello everybody c'est vendredi bientôt en week-end</h1>
 		<h2> VIvement ce soir</h2>
 		<p>Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end </P>
+		<h2> 2è paragraphe</h2>
+		<p>Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end Hello everybody c'est vendredi bientôt en week-end </P>
 	</body>
 </html>
 
